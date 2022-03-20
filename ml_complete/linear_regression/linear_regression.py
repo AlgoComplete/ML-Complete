efe9611f53e433befb5faaa38e1e72536c8af111
IndraP24 <indrashis985@gmail.com>
2022-03-20 19:08:28 +0530

    Code Linting following PEP8 format

--- ml_complete/linear_regression/linear_regression.py
+++ ml_complete/linear_regression/linear_regression.py
@@ -1,8 +1,6 @@
-# Author: Indrashis Paul
-# Date: 15-03-2022
+"""This script implements the Linear Regression Algorithm from scratch."""
 
 # importing required libraries
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,17 +8,20 @@ import matplotlib.pyplot as plt
 # Constructing the linear regression model
 
 class LinearRegression:
+    # pylint: disable=too-many-instance-attributes
+    """Linear Regression Class"""
+
     def __init__(self, learning_rate=0.05, iterations=1000):
         """
-        Linear Regression
+        Linear Regression Constructor
 
         Args:
             learning_rate (float, optional): Defaults to 0.05.
             iterations (int, optional): Defaults to 1000.
 
         Functions:
-            fit : (X, y) -> Train the data
-            predict : (X) -> Predict the new data from the previous trained model
+            fit : (data, label) -> Train the data
+            predict : (data) -> Predict the new label from the new data
 
         """
         self.learning_rate = learning_rate
@@ -28,72 +29,73 @@ class LinearRegression:
         self.weights = None
         self.bias = None
 
-    def fit(self, X, y):
+    def fit(self, data, label):
         """
-        This function takes the X and y as input and trains the model based on the defined classmethods.
+        This function trains a linear regression model.
 
         Parameters
         ----------
-            X : array-like, shape = [n_samples, n_features]
-                Training vectors, where n_samples is the number of samples and n_features is the number of features.
-            y : array-like, shape = [n_samples, n_target_values]
-                Target values, where n_samples is the number of samples and n_target_values is the number of target values.
+            data : array-like, shape = [n_samples, n_features]
+                Training vectors, with n_samples  and n_features.
+            label : array-like, shape = [n_samples, n_target_values]
+                Target values, with n_samples and n_target_values.
 
         Returns
         -------
             self : object
         """
-        n_samples, n_features = X.shape
+        n_samples, n_features = data.shape
 
         # init parameters
         self.weights, self.bias = np.zeros(n_features), 0
 
         # Gradient Descent
-        for i in range(self.iterations):
+        for _ in range(self.iterations):
             # Linear forward propagation
-            y_pred = np.dot(X, self.weights) + self.bias
+            label_pred = np.dot(data, self.weights) + self.bias
 
             # Compute gradients
-            grad_weights = (1 / n_samples) * np.dot(X.T, (y_pred - y))
-            grad_bias = (1 / n_samples) * np.sum(y_pred - y)
+            grad_weights = (1 / n_samples) * \
+                np.dot(data.T, (label_pred - label))
+            grad_bias = (1 / n_samples) * np.sum(label_pred - label)
 
             # Update parameters
             self.weights -= self.learning_rate * grad_weights
             self.bias -= self.learning_rate * grad_bias
 
-    def predict(self, X):
-        """ Predict the new data from the previous trained model
+    def predict(self, data):
+        """ Predict the new data from the previously trained model
 
         Parameters
         ----------
-            X : array-like, shape = [n_samples, n_features]
-                Testing vectors, where n_samples is the number of samples and n_features is the number of features.
+            data : array-like, shape = [n_samples, n_features]
+                Testing vectors, with n_samples and n_features.
 
         Returns
         -------
-            y_pred : array-like, shape = [n_samples, n_target_values]
+            label_pred : array-like, shape = [n_samples, n_target_values]
                 Predicted values using the trained linear model.
         """
-        return np.dot(X, self.weights) + self.bias
+        return np.dot(data, self.weights) + self.bias
 
-    def r2_score(self, X, y):
+    def r2_score(self, data, label):
         """ Compute the R2 score of the model
 
         Parameters
         ----------
-            X : array-like, shape = [n_samples, n_features]
-                Training/Testing vectors, where n_samples is the number of samples and n_features is the number of features.
-            y : array-like, shape = [n_samples, n_target_values]
-                Training/Testing target values, where n_samples is the number of samples and n_target_values is the number of target values.
+            data : array-like, shape = [n_samples, n_features]
+                Training/Testing vectors, with n_samples and n_features.
+            label : array-like, shape = [n_samples, n_target_values]
+                Training/Testing target values, with n_samples and n_features.
 
         Returns
         -------
             r2_score : float
                 R2 score of the trained model on the provided data.
         """
-        y_pred = self.predict(X)
-        corr_matrix = np.corrcoef(y, y_pred)
-        corr = corr_matrix[0, 1]
+        label_pred = self.predict(data)
+        corr_matridata = np.corrcoef(label, label_pred)
+        corr = corr_matridata[0, 1]
         return corr**2
 
 
@@ -107,41 +109,41 @@ if __name__ == "__main__":
     n_features = int(input("Enter the number of features: "))
 
     # Prepare the data
-    X, y = datasets.make_regression(
+    data, label = datasets.make_regression(
         n_samples=n_sample, n_features=n_features, noise=10, random_state=4
     )
-    print(X.shape, y.shape)
+    print(data.shape, label.shape)
 
     # Split the data into training and testing
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0
+    data_train, data_test, label_train, label_test = train_test_split(
+        data, label, test_size=0.2, random_state=0
     )
 
     # Train the model
     model = LinearRegression(0.05, 1000)
-    model.fit(X_train, y_train)
+    model.fit(data_train, label_train)
 
     # Predict the values
-    y_pred = model.predict(X_test)
+    label_pred = model.predict(data_test)
 
     # Compute the R2 score
-    r2_score = model.r2_score(X_test, y_test)
+    r2_score = model.r2_score(data_test, label_test)
     print("R2 score: ", r2_score)
 
     # Plot the graph for data with 1 feature
-    if (X.shape[1] == 1):
+    if data.shape[1] == 1:
         # Plot the data
         cmap = plt.get_cmap("viridis")
         fig = plt.figure(figsize=(8, 6))
         plt.scatter(
-            X_train, y_train, color=cmap(0.9), s=10, label="Training Data")
+            data_train, label_train, color=cmap(0.9), s=10, label="Train Data")
         plt.scatter(
-            X_test, y_test, color=cmap(0.5), s=10, label="Testing Data")
+            data_test, label_test, color=cmap(0.5), s=10, label="Test Data")
         plt.plot(
-            X_test, y_pred, color="red", label="Prediction")
-        plt.xlabel("X - Features", fontsize=18)
-        plt.ylabel("y - Outcome", fontsize=18)
+            data_test, label_pred, color="red", label="Prediction")
+        plt.datalabel("data - Features", fontsize=18)
+        plt.ylabel("label - Outcome", fontsize=18)
         plt.legend()
         plt.show()
     else:
-        print("Plotting the data is not possible for more than 1 feature in a 2-D plot")
+        print("Plotting not possible for more than 1 feature in a 2-D plot")
