4c8f6cd355ed4458f5dd83e3c9ceada232fd2e75
IndraP24 <indrashis985@gmail.com>
2022-03-20 19:42:18 +0530

    Update linting of code

--- ml_complete/linear_regression/linear_regression.py
+++ ml_complete/linear_regression/linear_regression.py
@@ -7,7 +7,6 @@ import numpy as np
 # Constructing the linear regression model
 
 class LinearRegression:
-    # pylint: disable=too-many-instance-attributes
     """Linear Regression Class"""
 
     def __init__(self, learning_rate=0.05, iterations=1000):
@@ -19,8 +18,8 @@ class LinearRegression:
             iterations (int, optional): Defaults to 1000.
 
         Functions:
-            fit : (data, label) -> Train the data
-            predict : (data) -> Predict the new label from the new data
+            fit : (X, y) -> Train the X
+            predict : (X) -> Predict the new y from the new X
 
         """
         self.learning_rate = learning_rate
@@ -28,22 +27,22 @@ class LinearRegression:
         self.weights = None
         self.bias = None
 
-    def fit(self, data, label):
+    def fit(self, X, y):
         """
         This function trains a linear regression model.
 
         Parameters
         ----------
-            data : array-like, shape = [n_samples, n_features]
+            X : array-like, shape = [n_samples, n_features]
                 Training vectors, with n_samples  and n_features.
-            label : array-like, shape = [n_samples, n_target_values]
+            y : array-like, shape = [n_samples, n_target_values]
                 Target values, with n_samples and n_target_values.
 
         Returns
         -------
             self : object
         """
-        n_samples, n_features = data.shape
+        n_samples, n_features = X.shape
 
         # init parameters
         self.weights, self.bias = np.zeros(n_features), 0
@@ -51,48 +50,48 @@ class LinearRegression:
         # Gradient Descent
         for _ in range(self.iterations):
             # Linear forward propagation
-            label_pred = np.dot(data, self.weights) + self.bias
+            y_pred = np.dot(X, self.weights) + self.bias
 
             # Compute gradients
             grad_weights = (1 / n_samples) * \
-                np.dot(data.T, (label_pred - label))
-            grad_bias = (1 / n_samples) * np.sum(label_pred - label)
+                np.dot(X.T, (y_pred - y))
+            grad_bias = (1 / n_samples) * np.sum(y_pred - y)
 
             # Update parameters
             self.weights -= self.learning_rate * grad_weights
             self.bias -= self.learning_rate * grad_bias
 
-    def predict(self, data):
-        """ Predict the new data from the previously trained model
+    def predict(self, X):
+        """ Predict the new X from the previously trained model
 
         Parameters
         ----------
-            data : array-like, shape = [n_samples, n_features]
+            X : array-like, shape = [n_samples, n_features]
                 Testing vectors, with n_samples and n_features.
 
         Returns
         -------
-            label_pred : array-like, shape = [n_samples, n_target_values]
+            y_pred : array-like, shape = [n_samples, n_target_values]
                 Predicted values using the trained linear model.
         """
-        return np.dot(data, self.weights) + self.bias
+        return np.dot(X, self.weights) + self.bias
 
-    def r2_score(self, data, label):
+    def r2_score(self, X, y):
         """ Compute the R2 score of the model
 
         Parameters
         ----------
-            data : array-like, shape = [n_samples, n_features]
+            X : array-like, shape = [n_samples, n_features]
                 Training/Testing vectors, with n_samples and n_features.
-            label : array-like, shape = [n_samples, n_target_values]
+            y : array-like, shape = [n_samples, n_target_values]
                 Training/Testing target values, with n_samples and n_features.
 
         Returns
         -------
             r2_score : float
-                R2 score of the trained model on the provided data.
+                R2 score of the trained model on the provided X.
         """
-        label_pred = self.predict(data)
-        corr_matridata = np.corrcoef(label, label_pred)
-        corr = corr_matridata[0, 1]
+        y_pred = self.predict(X)
+        corr_matriX = np.corrcoef(y, y_pred)
+        corr = corr_matriX[0, 1]
         return corr**2
--- ml_complete/logistic_regression/logistic_regression.py
+++ ml_complete/logistic_regression/logistic_regression.py
@@ -1,17 +1,14 @@
-# Author: Indrashis Paul
-# Date: 15-03-2022
+"""This script implements the Logistic Regression Algorithm from scratch."""
 
 # importing required libraries
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn.metrics import accuracy_score
 
 
 # Constructing the Logistic regression model
 
 class LogisticRegression:
+    """Logistic Regression Class"""
+    
     def __init__(self, learning_rate=0.05, iterations=1000):
         """
         Logistic Regression
@@ -21,8 +18,8 @@ class LogisticRegression:
             iterations (int, optional): Defaults to 1000.
 
         Functions:
-            fit : (X, y) -> Train the data
-            predict : (X) -> Predict the new data from the previous trained model
+            fit : (X, y) -> Train the X
+            predict : (X) -> Predict the new y from the new X
 
         """
         self.learning_rate = learning_rate
@@ -32,14 +29,14 @@ class LogisticRegression:
 
     def fit(self, X, y):
         """
-        This function takes the X and y as input and trains the model based on the defined classmethods.
+        This function trains a logistic regression model.
 
         Parameters
         ----------
             X : array-like, shape = [n_samples, n_features]
-                Training vectors, where n_samples is the number of samples and n_features is the number of features.
+                Training vectors, with n_samples  and n_features.
             y : array-like, shape = [n_samples, n_target_values]
-                Target values, where n_samples is the number of samples and n_target_values is the number of target values.
+                Target values, with n_samples and n_target_values.
 
         Returns
         -------
@@ -67,82 +64,53 @@ class LogisticRegression:
             self.bias -= self.learning_rate * grad_bias
 
     def predict(self, X):
+        """ Predict the new X from the previously trained model
+
+        Parameters
+        ----------
+            X : array-like, shape = [n_samples, n_features]
+                Testing vectors, with n_samples and n_features.
+
+        Returns
+        -------
+            y_pred : array-like, shape = [n_samples, n_target_values]
+                Predicted values using the trained linear model.
+        """
         linear_model = np.dot(X, self.weights) + self.bias
         y_pred = self.sigmoid(linear_model)
         pred_class = np.where(y_pred >= 0.5, 1, 0)
         return np.array(pred_class, dtype=int)
 
     def sigmoid(self, z):
+        """ Compute the sigmoid function of the input z
+        
+        Parameters
+        ----------
+            z : array-like, shape = [n_samples, n_features]
+                Input values.
+        
+        Returns
+        -------
+            sigmoid : array-like, shape = [n_samples, n_features]
+                Sigmoid values of the input z.
+        """
         return 1 / (1 + np.exp(-z))
 
     def accuracy_score(self, X, y):
+        """ Compute the Accuracy Score of the model
+
+        Parameters
+        ----------
+            X : array-like, shape = [n_samples, n_features]
+                Training/Testing vectors, with n_samples and n_features.
+            y : array-like, shape = [n_samples, n_target_values]
+                Training/Testing target values, with n_samples and n_features.
+
+        Returns
+        -------
+            Accuracy_score : float
+                Accuracy Score of the trained model on the provided X.
+        """
         y_pred = self.predict(X)
         y_pred = np.round(y_pred)
         return np.mean(y == y_pred)
-
-
-# Testing
-if __name__ == "__main__":
-    # Imports
-    from sklearn.model_selection import train_test_split
-    from sklearn import datasets
-
-    n_sample = int(input("Enter the number of samples: "))
-    n_features = int(input("Enter the number of features: "))
-
-    n_redundant = 0
-    if n_features > 1:
-        n_redundant = 1
-
-    # Prepare the data
-    X, y = datasets.make_classification(
-        n_samples=n_sample, n_features=n_features, n_classes=2, n_informative=1, n_redundant=n_redundant, n_repeated=0, n_clusters_per_class=1, random_state=4
-    )
-
-    print(X.shape, y.shape)
-
-    # Split the data into training and testing
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0
-    )
-
-    # Train the model
-    model = LogisticRegression(0.05, 1000)
-    model.fit(X_train, y_train)
-
-    # Predict the values
-    y_pred = model.predict(X_test)
-
-    # Compute the R2 score
-    accuracy_score = model.accuracy_score(X_test, y_test)
-    print("Accuracy score: ", accuracy_score)
-
-    def plot_data(x, y):
-        plt.xlabel('score of test-1')
-        plt.ylabel('score of test-1')
-        for i in range(x.shape[0]):
-            if y[i] == 1:
-                plt.plot(x[i, 0], x[i, 1], 'gX')
-            else:
-                plt.plot(x[i, 0], x[i, 1], 'mD')
-        plt.show()
-
-    plot_data(X_test, y_test)
-
-    # Plot the graph for data with 1 feature
-    if (X.shape[1] == 1):
-        # Plot the data
-        cmap = plt.get_cmap("viridis")
-        fig = plt.figure(figsize=(8, 6))
-        plt.scatter(
-            X_train, y_train, color=cmap(0.9), s=10, label="Training Data")
-        plt.scatter(
-            X_test, y_test, color=cmap(0.5), s=10, label="Testing Data")
-        plt.plot(
-            X_test, y_pred, color="red", label="Prediction")
-        plt.xlabel("X - Features", fontsize=18)
-        plt.ylabel("y - Outcome", fontsize=18)
-        plt.legend()
-        plt.show()
-    else:
-        print("Plotting the data is not possible for more than 1 feature in a 2-D plot")
